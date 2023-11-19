aa6e3bef150f7b40da9dacd7ae76c5e2ab78e8d7
ErwanBarbedor <erwan@barbedor.bzh>
2023-11-20 00:23:41 +0100

    update license

--- plume.lua
+++ plume.lua
@@ -1,6 +1,28 @@
+--[[
+Plume Templating Langage v1.0.0-alpha
+Copyright (C) 2023  Erwan Barbedor
+
+Check https://github.com/ErwanBarbedor/Plume-Templating-Langage
+for documentation, tutorial or to report issues.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see https://www.gnu.org/licenses/.
+]]
+
 local Plume = {}
 
--- Empty function used as a placeholder for compatibility
+Plume._VERSION = "Plume Templating Langage 1.0.0-alpha"
+
+-- Empty function used as a placeholder for lua 5.1 compatibility
 local function dummy () end
 
 -- Predefined list of standard Lua variables/functions for various versions
