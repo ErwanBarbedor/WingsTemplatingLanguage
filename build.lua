7ca06d05598b0703edcf9666991c1d61a19d526a
ErwanBarbedor <erwan@barbedor.bzh>
2023-11-24 00:06:28 +0100

    change version name

--- build.lua
+++ build.lua
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License along with Lua
 print("Creating plume.lua...")
 -- Merge all plume code into a single standalone file
 local plume   = io.open 'main.lua':read '*a'
-local version = 'v1.0.0'
+local version = 'LuaPlume v1.0.0'
 local alpha   = true
 
 if alpha then
